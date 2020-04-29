53f7e81a28d20613f7d90bfc0acf2ec4c4f8bc74
Yevgeniya <ekim27220@gmail.com>
2020-04-30 04:15:02 +0600

    done

--- SeProject/src/app/models/categories.ts
+++ SeProject/src/app/models/categories.ts
@@ -1,26 +1,32 @@
 export interface Category {
   name: string;
   link: string;
+  img: string;
 }
 
 export const categories: Category[] = [
   {
     name: 'Faculties',
-    link: '/faculties'
+    link: '/faculties',
+    img: 'assets/software/Организации/facultyIcon.png'
   },
 
   {
     name: 'Organisations',
-    link: '/organisations'
+    link: '/organisations',
+    img: 'assets/software/Организации/organisationsIcon.png'
   },
 
   {
     name: 'Map',
-    link: '/map'
+    link: '/map',
+    img: 'assets/software/Организации/mapIcon.png'
   },
 
   {
     name: 'Contact us',
-    link: '/contacts'
+    link: '/contacts',
+    img: 'assets/software/Организации/contactIcon.png'
   }
 ];
+
--- SeProject/src/app/models/organisations.ts
+++ SeProject/src/app/models/organisations.ts
@@ -1,21 +1,35 @@
 export interface Organisation {
   id: number;
   name: string;
+  img: string;
+  back: string;
 }
 
 export const organisations: Organisation[] = [
   {
     id: 21,
     name: 'Alive',
+    img: 'assets/software/Организации/icon.png',
+    back: 'assets/software/Организации/Info Alive.png'
   },
 
   {
     id: 22,
     name: 'Crystal',
+    img: 'src/assets/software/Меню/Faculties.png',
+    back: 'assets/software/Организации/Info Crystal.png'
   },
 
   {
     id: 23,
     name: 'BigCityLights',
+    img: 'src/assets/software/Меню/Faculties.png',
+    back: 'assets/software/Организации/Info BSL.png'
+  },
+  {
+    id: 24,
+    name: 'Enactus',
+    img: 'src/assets/software/Меню/Faculties.png',
+    back: 'assets/software/Организации/Info enactus.png'
   },
 ];
